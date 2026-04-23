ba4612c32d75cca438748c55abdfa5bcc71cec36
Maan Patel <maance142@email.com>
2026-04-23 15:58:05 +0530

    Area = sqrt(s*(s-a)*(s-b)*(s-c)) where s = (a+b+c)/2)

--- .vscode/CO1_3.java
+++ .vscode/CO1_3.java
@@ -4,7 +4,6 @@ public class CO1_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Prompt user
         System.out.print("Enter a single letter: ");
         char ch = sc.next().charAt(0);
 
